7be0fbd7a561bf96214a4f8964438e5f8061c8bc
BAEK <100k1@naver.com>
2022-02-15 13:00:36 +0900

    인풋 태그 풋데이터 동시 출력하기

--- usestate-input-event/src/App.js
+++ usestate-input-event/src/App.js
@@ -1,9 +1,32 @@
 import React, { useState } from 'react';
 
 function App() {
+
+  const [name,setName] = useState('백광일');
+  const [nickname, setNickname] = useState('닉네임');
+
+  const onChangeName = e => {
+    setName(e.target.value);
+  }
+
+  const onChangeNickname = e => {
+    setNickname(e.target.value);
+  }
+
   return (
-    <div className="App">
- 
+    <div >
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
     </div>
   );
 }
